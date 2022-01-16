8c272f1b6accbb95d10157185b962e3cca8a8a5b
johmert <johmert@users.noreply.github.com>
2022-01-15 18:34:40 -0800

    react-route practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { 
+  BrowserRouter as Router,
+  Link, 
+  Route, 
+  Switch,
+  useLocation } from "react-router-dom";
+
+function Home() {
+  return <p>Home</p>
+}
+
+function About() {
+  return <p>About</p>
+}
+
+function NoMatch() {
+  const location = useLocation();
+  return (
+    <h3>
+      No match for <code>{location.pathname}</code>
+    </h3>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Switch>
+          <Route exact={true} path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
